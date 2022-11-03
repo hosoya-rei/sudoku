957bc2e7d473466fc339f60893a2d32c3c42da74
Rei Hosoya <reirei.hoso.boz@gmail.com>
2022-11-03 07:02:14 +0000

    ナンプレ9かけ9実装

--- main.c
+++ main.c
@@ -4,24 +4,38 @@
 #include <time.h>
 
 
-#define SIZE 4
+#define SIZE 9
+struct sudoku {
+	int a[81]; 
+	int a2[16]; 
+	int a3[16]; 
+	int a4[16]; 
+	int a5[16];
+	char b[81]; 
+	char b2[16]; 
+	char b3[16]; 
+	char b4[16]; 
+	char b5[16];
+};
 int i, k, j;
-void init_game(int a[], char b[], int ans[][SIZE], char bb[][SIZE]);
 void put_table(char bb[][SIZE]);
 int end_judge(char bb[][SIZE]);
-int true_folse(char bb[][SIZE], int a[]);
+int true_folse(char bb[][SIZE], struct sudoku s);
+void init_game(struct sudoku s, int ans[][SIZE], char bb[][SIZE]);
+void add_sudoku(struct sudoku *s);
 
 
 
 int main(void) {
-	int a[] = {2, 3, 4, 1, 4, 1, 2, 3, 3, 4, 1, 2, 1, 2, 3, 4};
-	char b[] = {'.', '.', '.', '1', '4', '.', '.', '3', '.', '.', '.', '.', '1', '2', '.', '4'};
+	struct sudoku su;	
+	
 	int ans[SIZE][SIZE];
 	char bb[SIZE][SIZE];
 	int x, y, num;
+	add_sudoku(&su);
 
 	printf("sudoku!!\n");
-	init_game(a, b, ans, bb);
+	init_game(su, ans, bb);
 	
 	while (1) {
 		printf("1~4の座標と入れる数字を入力(x:横, y:縦)\n");
@@ -31,7 +45,7 @@ int main(void) {
 		printf("y="); scanf("%d", &y);
 		printf("num="); scanf("%d", &num);
 		
-		if (x > 0 && x < 5 && y > 0 && y < 5) {
+		if (x > 0 && x <= SIZE && y > 0 && y <= SIZE) {
 			bb[y - 1][x - 1] = '0' + num;
 		}else {
 			printf("不正な数値です。\n");
@@ -41,7 +55,7 @@ int main(void) {
 		put_table(bb);
 		
 		if (end_judge(bb) == 0) {
-			if (true_folse(bb, a) == 16) {
+			if (true_folse(bb, su) == SIZE * SIZE) {
 				printf("クリア！！\n");
 			}else {
 				printf("失敗です。\n");
@@ -53,13 +67,24 @@ int main(void) {
 return 0;	
 }
 
-int true_folse(char bb[][SIZE], int a[]) {
+void add_sudoku(struct sudoku *s) {
+	char a_tenta[SIZE * SIZE] = {7, 1, 2, 9, 8, 4, 3, 5, 6, 5, 3, 4, 1, 6, 7, 2, 8, 9, 9, 6, 8, 3, 5, 2, 7, 1, 4, 2, 8, 1, 5, 3, 9, 6, 4, 7, 6, 7, 3, 2, 4, 1, 5, 9, 8, 4, 5, 9, 6, 7, 8, 1, 2, 3, 8, 4, 5, 7, 2, 6, 9, 3, 1, 1, 2, 6, 4, 9, 3, 8, 7, 5, 3, 9, 7, 8, 1, 5, 4, 6, 2};
+	char b_tenta[SIZE * SIZE] = {'7', '1', '2', '.', '.', '4', '.', '5', '6', '.', '3', '4', '1', '.', '.', '.', '8', '9', '.', '.', '8', '3', '5', '2', '.', '.', '.', '.', '.', '1', '.', '3', '9', '6', '4', '7', '6', '7', '.', '2', '.', '.', '5', '.', '.', '4', '5', '.', '.', '.', '8', '1', '2', '3', '8', '4', '5', '.', '.', '6', '9', '.', '1', '.', '.', '6', '4', '9', '.', '.', '.', '.', '3', '9', '.', '8', '1', '5', '.', '.', '2'};
+
+	for (i = 0; i < SIZE * SIZE; i++) {
+		s->a[i] = a_tenta[i];
+		s->b[i] = b_tenta[i];
+	}
+}
+
+
+int true_folse(char bb[][SIZE], struct sudoku s) {
 	j = 0;
 	char test[5];
 	int count = 0;
 	for (i = 0; i < SIZE; i++) {
 		for (k = 0; k < SIZE; k++) {
-			test[0] = '0' + a[j++];
+			test[0] = '0' + s.a[j++];
 			if (bb[i][k] == test[0]) {
 				count++;
 			}
@@ -84,28 +109,36 @@ return count;
 
 
 void put_table(char bb[][SIZE]) {
-	printf("\n  1 2 3 4\n");
-	printf("  ーーーー\n");	
+	printf("\n  1 2 3  4 5 6  7 8 9\n");
+	printf("  ーーーーーーーーーー\n");	
 	for (i = 0; i < SIZE; i++) {
 		printf("%d|", i + 1);
 		for (k = 0; k < SIZE; k++) {
 			printf("%c ", bb[i][k]);
+			if (k != 8) {
+				if ((k + 1) % 3 == 0) {
+					printf("|");
+				}
+			}
 		}printf("|\n");
-	}printf("  ーーーー\n");
+	if ((i + 1) % 3 == 0) {
+		printf("  ーーーーーーーーーー\n");
+	}
+	}
 }
 		
 
 
-void init_game(int a[], char b[], int ans[][SIZE], char bb[][SIZE]) {
+void init_game(struct sudoku s, int ans[][SIZE], char bb[][SIZE]) {
 	for (i = 0; i < SIZE; i++) {
 		for (k = 0; k < SIZE; k++) {
-			ans[i][k] = a[j++];
+			ans[i][k] = s.a[j++];
 		}
 	}
 	j = 0;
 	for (i = 0; i < SIZE; i++) {
 		for (k = 0; k < SIZE; k++) {
-			bb[i][k] = b[j++];		 	
+			bb[i][k] = s.b[j++];		 	
 		}
 	}
 }
